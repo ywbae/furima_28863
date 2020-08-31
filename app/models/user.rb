5029668583e13bcb4ea1ffc3468b89019e4fad65
YongWan Bae <yonwan.b@gmail.com>
2020-08-31 16:24:27 +0900

    rubocop自動修正実行後

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :birth_date, presence: true 
+  validates :nickname, :birth_date, presence: true
   with_options presence: true do
-    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-    validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください。' }
+    validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' }
     validates :email, :nickname, uniqueness: true
   end
 end
