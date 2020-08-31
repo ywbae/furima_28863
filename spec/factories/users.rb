54fc32a51fa30156c3fd135630f28f635bcb8dd2
YongWan Bae <yonwan.b@gmail.com>
2020-08-31 12:44:19 +0900

    pry-rails 導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"abe"}
+    email                 {"kkk@gmail.com"}
+    password              {"00000000"}
+    password_confirmation {password}
+  end
+end
