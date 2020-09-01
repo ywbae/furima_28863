10a19aede025707393648a65d8a261d11b06874e
YongWan Bae <yonwan.b@gmail.com>
2020-09-01 09:43:11 +0900

    itemsコントローラー、activehashバリデーション、出品ボタンのリンク、ラウティング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :price, :trading_status, :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,16 @@ class Item < ApplicationRecord
   belongs_to :users
   has_many :comments
   has_one :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :sales_status
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :scheduled_delivery
+
+  validates :name, :text, :price, :trading_status, :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, presence: true
+
+  validates :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 }
 end
+
