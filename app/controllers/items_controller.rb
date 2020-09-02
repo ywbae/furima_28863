8ba542bc0f480cbe67047a0bc04ae37b5ce536b3
YongWan Bae <yonwan.b@gmail.com>
2020-09-02 11:51:23 +0900

    editアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :destroy]
+  before_action :set_item, only: [:show, :destroy, :edit]
 
   def index
   end
@@ -29,6 +29,8 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
   private
 
   def item_params
