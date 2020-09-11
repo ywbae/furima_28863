168dc173df019b8193c5a2f5a2b65073a7a2468d
YongWan Bae <yonwan.b@gmail.com>
2020-09-11 19:27:18 +0900

    コントローラー、payment_form修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,19 @@
 class OrdersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
   def index
     @item = Item.find(params[:item_id])
+    @order = PaymentForm.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order = PaymentForm.new(order_params)
+    @order.save
+  end
+
+  private
+
+  def order_params
+    params.permit(:item_id, :order_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id)
   end
 end
--- app/forms/payment_form.rb
+++ app/forms/payment_form.rb
@@ -1,22 +1,21 @@
 class PaymentForm
-
   include ActiveModel::Model  
-  attr_accessor :user_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order_id
+  attr_accessor :user_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'を入力してください' }
     validates :prefecture_id, numericality: { other_than: 0, message: 'を選択してください' }
     validates :city
-    validates :address
+    validates :addresses
     validates :phone_number, format: { with: /\A\d{,11}\z/, message: 'は11桁以内の数字（ハイフン不要）で入力してください' }
   end
 
   def save
-    order = Order.create(item_id: item_id, user_id: user_id)
-    Shipping_addresses.create(
-      postal_code: postal_code, prefecture_id: prefecture_id,
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(
+      postal_code: postal_code, prefecture: prefecture_id,
       city: city, addresses: addresses, building: building,
-      phone_number: phone_number
+      phone_number: phone_number, order_id: order.id
     )
   end
 end
