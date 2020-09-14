d59f6f360977f901067f570c47796dc0903d20c4
YongWan Bae <yonwan.b@gmail.com>
2020-09-15 04:19:19 +0900

    遷移制限記載

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,11 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    if user_signed_in?
+      redirect_to root_path unless current_user.id != @item.user_id
+    else
+      redirect_to root_path
+    end
     @order = PaymentForm.new
   end
 
