f503d577a471006944611326f1d0009bbdf8f6d4
YongWan Bae <yonwan.b@gmail.com>
2020-09-15 04:17:02 +0900

    payment単体テストコード

---
+++ spec/factories/payment_forms.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :payment_form do
+    token {'testcard'}
+    postal_code {'111-1234'}
+    prefecture_id {12}
+    city {'練馬区'}
+    addresses {'青山1-1-3'}
+    phone_number {'07011111234'}
+  end
+end
